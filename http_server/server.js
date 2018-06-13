3dbfa56346882f768b36bb3879718c7460fafbeb
Alex Wilmer <djazium@gmail.com>
2018-06-13 09:45:39 +0000

    🌎 get public ip

--- http_server/server.js
+++ http_server/server.js
@@ -1,11 +1,26 @@
-let express = require('express')
+// thingy to get server ip address
+let publicIp = require('public-ip')
 
+// express === node's goto http server
+let express = require('express')
 let app = express()
 
-app.get('/', (request, response) => {
+// PORT from environment variable with default
+let port = process.env.PORT || 8080
+
+// send a string to all requestors 
+app.get('*', (request, response) => {
   response.send(`hey hey hey, I'm an express server yay`)
 })
 
-app.listen(8080, () => {
-  console.log(`⚡️ server is active! ⚡️`)
+// get the server's public ip
+publicIp.v4().then(ip => {
+  // activate the server (will hang the node process)
+  app.listen(port, () => {
+    console.log(`
+     ⚡️ server activated! ⚡️
+     get a string from http://${ip}:${port}
+    `)
+  })
 })
+
